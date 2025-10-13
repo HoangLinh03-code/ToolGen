1ccbce617ffc180d290c081280c33efbf5be5791
unknown <nguyentungduong290403@gmail.com>
2025-10-13 16:59:29 +0700

    fix: import .env file when build tool

--- GenQues.py
+++ GenQues.py
@@ -27,6 +27,14 @@ from process.response2docx import response2docx_improved
 
 load_dotenv()
 
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(__file__)
+
+dotenv_path = os.path.join(base_path, '.env')
+load_dotenv(dotenv_path)
+
 
 # ==================== CONSOLE REDIRECT ====================
 class ConsoleRedirect(QObject):
--- process/text2Image.py
+++ process/text2Image.py
@@ -1,8 +1,17 @@
-import os
+import os,sys
 import vertexai
 from vertexai.preview.vision_models import ImageGenerationModel
 from api.callAPI import get_vertex_ai_credentials
 import io
+from dotenv import load_dotenv
+
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(__file__)
+
+dotenv_path = os.path.join(base_path, '.env')
+load_dotenv(dotenv_path)
 
 def generate_image_from_text(prompt):
     # Khởi tạo Vertex AI với credentials từ .env
